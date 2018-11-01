8ded1f4672c656165ef1fa5b57e8c12a815d06ac
AdGw <adrian.gwozdziej@gmail.com>
2018-11-01 17:58:45 +0100

    add this examples

---
+++ JavaScript/ECMA6/This/this.js
@@ -0,0 +1,47 @@
+function welcome(){
+	console.log(`Hello: ${this.name}`);
+}
+
+let users = [
+
+	{name: "a"},
+	{name: "b"},
+	{name: "c"},
+	{name: "d"}
+]
+
+users.forEach(user => welcome.call(user))
+
+function main(){
+	for(let i in users){
+		console.log("Hello:",users[i].name)
+	}
+}
+
+main();
+
+////
+
+let article = {title: "This"}
+
+function fetchData(){
+	console.log(this.title)
+}
+
+fetchData.call(article)
+fetchData.apply(article)
+
+let bound = fetchData.bind(article);
+bound();
+
+class Article{
+	contructor(title){
+		this.title = title
+	}
+	getTitle(){
+		console.log(this)
+	}
+}
+
+let book = new Article('x')
+book.getTitle();
