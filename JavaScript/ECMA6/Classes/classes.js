c6d6d12e2cdcd60520a6798a2d35cb990baf9b4d
AdGw <adrian.gwozdziej@gmail.com>
2018-11-07 17:25:13 +0100

    modify classes file

--- JavaScript/ECMA6/Classes/classes.js
+++ JavaScript/ECMA6/Classes/classes.js
@@ -1,28 +1,5 @@
-
-// class Media{
-// 	constructor(type, name, author){
-// 		this.type = type;
-// 		this.name = name;
-// 		this.author = author;
-// 	}
-// 	getFullName(){
-// 		return `${this.name} (${this.author})`
-// 	}
-// }
-
-// class Song extends Media{
-// 	constructor(name, author){
-// 		super('song', name, author);
-
-// 	}
-// }
-
-// const song = new Song("Let It Be", "The Beatles")
-// console.log(song.getFullName())
-
-
 class Media{
-	constructor(type, name, author){
+	constructor(type, name){
 		this.type = type;
 		this.name = name;
 	}
